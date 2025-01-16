441664886544ae388cd44571fc733625dc6825d6
manojbishwakarma <manojbishwakarma@neebal.com>
2025-01-15 22:45:38 -0500

    pushing Updating the changes

--- script.js
+++ script.js
@@ -6,11 +6,11 @@ searchButton.addEventListener("click", () => {
   const query = searchInput.value.trim();
   if (query) {
     fetch(
-      `https://api.giphy.com/v1/gifs/search?api_key=YOUR_API_KEY&q=${query}&limit=10`
+      `https://api.giphy.com/v1/gifs/search?api_key=mXPyH2xJIbwtaoeYPwzoboVjA3NspEOz&q=${query}&limit=10`
     )
       .then((response) => response.json())
       .then((data) => {
-        gifContainer.innerHTML = ""; // Clear previous results
+        gifContainer.innerHTML = "";
         data.data.forEach((gif) => {
           const img = document.createElement("img");
           img.src = gif.images.fixed_height.url;
